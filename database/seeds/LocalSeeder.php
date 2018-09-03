053f409fb74dfa70f1bcdc82458af39e929b42ac
John <jqm004@alumnos.ucn.cl>
2018-09-03 01:24:42 -0300

    Added: Some parking address

--- database/seeds/LocalSeeder.php
+++ database/seeds/LocalSeeder.php
@@ -42,5 +42,65 @@ class LocalSeeder extends Seeder
             'hora_aten_ter' => '22:30',
             'direccion' => 'direccion de prueba 3'
         ]);
+
+        \App\local::create([
+            'coor_x' => -23.6688405,
+            'coor_y' => -70.4044434,
+            'cant_est' => 20,
+            'cant_disp' => 7,
+            'hora_aten_ini' => '08:00',
+            'hora_aten_ter' => '22:30',
+            'direccion' => 'direccion de prueba 4'
+        ]);
+
+        \App\local::create([
+            'coor_x' => -23.6818139,
+            'coor_y' => -70.4105976,
+            'cant_est' => 20,
+            'cant_disp' => 7,
+            'hora_aten_ini' => '08:00',
+            'hora_aten_ter' => '22:30',
+            'direccion' => 'direccion de prueba 5'
+        ]);
+
+        \App\local::create([
+            'coor_x' => -23.6833793,
+            'coor_y' => -70.4126739,
+            'cant_est' => 20,
+            'cant_disp' => 7,
+            'hora_aten_ini' => '08:00',
+            'hora_aten_ter' => '22:30',
+            'direccion' => 'direccion de prueba 6'
+        ]);
+
+        \App\local::create([
+            'coor_x' => -23.6821317,
+            'coor_y' => -70.4101993,
+            'cant_est' => 20,
+            'cant_disp' => 7,
+            'hora_aten_ini' => '08:00',
+            'hora_aten_ter' => '22:30',
+            'direccion' => 'direccion de prueba 7'
+        ]);
+
+        \App\local::create([
+            'coor_x' => -23.6814012,
+            'coor_y' => -70.4133363,
+            'cant_est' => 20,
+            'cant_disp' => 7,
+            'hora_aten_ini' => '08:00',
+            'hora_aten_ter' => '22:30',
+            'direccion' => 'direccion de prueba 8'
+        ]);
+
+        \App\local::create([
+            'coor_x' => -23.686551,
+            'coor_y' => -70.418518,
+            'cant_est' => 20,
+            'cant_disp' => 7,
+            'hora_aten_ini' => '08:00',
+            'hora_aten_ter' => '22:30',
+            'direccion' => 'direccion de prueba 9'
+        ]);
     }
 }
